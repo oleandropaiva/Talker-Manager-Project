7a5f4f8811b3ce74e02eab0c8885f8449c9d6543
Leandro Paiva <leandroapaiva@yahoo.com>
2022-10-08 20:48:05 -0300

    desenvolvimento de requisitos

--- middleware/index.js
+++ middleware/index.js
@@ -21,8 +21,74 @@ function validPassword(req, res, next) {
   }
   next();
 }
+function validAge(req, res, next) {
+  const { age } = req.body;
+  if (!age || age === ' ') {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (Number.isInteger(age) && age < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  next();
+}
+function validName(req, res, next) {
+  const { name } = req.body;
+  if (!name || name === ' ') {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  next();
+}
+
+function validToken(req, res, next) {
+  const { authorization } = req.headers;
+  if (!authorization || authorization === ' ') {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+  if (authorization.length < 16 || authorization.length > 16) {
+    return res.status(401).json({ message: 'Token inválido' });
+}
+next();
+}
+function validTalk(req, res, next) {
+  const { talk } = req.body;
+  if (!talk || talk === ' ') {
+    return res.status(400).json({ message: 'O campo "talk" é obrigatório' });
+  }
+  next();
+}
+const data = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+function validWatchedAt(req, res, next) {
+  const { watchedAt } = req.body.talk;
+
+  if (!watchedAt || watchedAt === ' ') { 
+    return res.status(400).json({ message: 'O campo "watchedAt" é obrigatório' });
+  }
+  if (!data.test(watchedAt)) {
+    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  next();
+}
+function validRate(req, res, next) {
+  const { rate } = req.body.talk;
+  if (rate < 1 || rate > 5) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro' });
+  }
+  if (!rate || rate === ' ') {
+    return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
+  }
+  next();
+}
 
 module.exports = {
   validEmail,
   validPassword,
+  validAge,
+  validName,
+  validToken,
+  validTalk,
+  validWatchedAt,
+  validRate,
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-
 const genToken = require('../middleware/genToken');
-const { validEmail, validPassword } = require('../middleware/index');
+const { validEmail, validPassword, validAge,
+  validName, validToken, validTalk, validWatchedAt, validRate } = require('../middleware/index');
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,7 +11,6 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 const talkerJson = './src/talker.json';
-
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -20,6 +19,15 @@ app.get('/', (_request, response) => {
 app.get('/talker', (req, res) => {
   const palestrantes = fs.readFileSync(talkerJson, 'utf-8');
   return res.status(HTTP_OK_STATUS).json(JSON.parse(palestrantes));
+});
+
+app.get('/talker/search', validToken, (req, res) => {
+  const { q } = req.query;
+  const palestrantes = fs.readFileSync(talkerJson, 'utf-8');
+  const resultBusca = JSON.parse(palestrantes).filter((palestrante) => 
+    palestrante.name.includes(q));
+
+  return res.status(200).json(resultBusca);
 });
 
 app.get('/talker/:id', (req, res) => {
@@ -41,6 +49,56 @@ app.get('/talker/:id', (req, res) => {
 app.post('/login', validEmail, validPassword, (req, res) => {
   const token = genToken();
   return res.status(HTTP_OK_STATUS).json({ token });
+});
+
+app.delete('/talker/:id', validToken, (req, res) => {
+  const { id } = req.params;
+  const palestrantes = JSON.parse(fs.readFileSync(talkerJson, 'utf-8'));
+  const filterTalkers = palestrantes.filter((talker) => talker.id !== Number(id));
+  fs.writeFileSync('./talker.json', JSON.stringify(filterTalkers));
+
+  return res.status(204).json();
+});
+
+app.use(validToken, validAge, validName, validTalk, validWatchedAt, validRate);
+
+app.post('/talker', (req, res) => {
+  const { age, name, talk } = req.body;
+  const { watchedAt, rate } = talk;
+  const genId = 5;
+  const talker = {
+    id: genId,
+    name,
+    age,
+    talk: {
+      watchedAt,
+      rate,
+    },
+  };
+  const palestrantes = JSON.parse(fs.readFileSync(talkerJson, 'utf8'));
+  palestrantes.push(talker);
+  fs.writeFileSync('./talker.json', JSON.stringify(palestrantes));
+  return res.status(201).json(talker);
+}); 
+
+app.put('/talker/:id', (req, res) => {
+  const { age, name, talk } = req.body;
+  const { watchedAt, rate } = talk;
+  const { id } = req.params;
+  const palestrantes = JSON.parse(fs.readFileSync(talkerJson, 'utf-8'));
+  palestrantes[Number(id) - 1] = {
+    ...palestrantes[Number(id) - 1],
+    id: Number(id),
+    name,
+    age,
+    talk: {
+      watchedAt,
+      rate,
+    },
+  };
+  fs.writeFileSync(talkerJson, JSON.stringify(palestrantes));
+
+  return res.status(200).json(palestrantes[Number(id) - 1]);
 });
 
 app.listen(PORT, () => {
